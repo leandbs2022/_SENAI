6296f309f22341a2e471e2a620b16d60aaf2e795
Leandro BSB <109764459+leandbs2022@users.noreply.github.com>
2024-05-14 11:07:23 -0300

    140520241107

--- Atividade/obj/Debug/net8.0/Atividade.AssemblyInfo.cs
+++ Atividade/obj/Debug/net8.0/Atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f884570cf8a38920070ebaf1b47eae5115370a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15cf8907625d7c0432bb116729c9ccd5795e31c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Pessoa_Fisica.cs
@@ -0,0 +1,9 @@
+namespace Atividades
+{
+    class pessoa_Fisica : Clientes
+    {
+        public string cpf{ get; set; }
+        public string rg{ get; set; }
+    }
+
+}
